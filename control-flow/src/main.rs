610cd6813e13537427d6477951a2c0f53e155855
Ethan Posey <100727989+nov8r@users.noreply.github.com>
2024-12-21 21:39:50 -0500

    Learning control flow

---
+++ control-flow/src/main.rs
@@ -0,0 +1,23 @@
+/*
+    Control flow
+*/
+
+
+
+/*
+    Control Flow: if Expressions
+*/
+
+fn if_func() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn main() {
+    if_func();
+}
