0e12b39945381dbae00aa66c7817757afa465d86
Ethan Posey <100727989+nov8r@users.noreply.github.com>
2024-12-21 21:13:12 -0500

    Added some stuff to functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,27 @@
+/*
+    To start a function we type `fn` followed by the function name
+    in my case it's `example_function` followed by parenthesis
+    and then a set of brackets so it knows where the function begins and ends.
+*/
+fn example_function() {
+    println!("I am a function.")
+}
+
+/*
+    Functions: Parameters
+*/
+
+// You must always define the type of the parameter
+fn function_value(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+// the main funciton is the entry point for programs in rust
+// meaning the program starts with this function
 fn main() {
     println!("Hello, world!");
+    
+    // here we call the example function we defined above
+    example_function();
+    function_value(78);
 }
